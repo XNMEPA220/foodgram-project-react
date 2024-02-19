03114d4f805e95f011fddd48f0571c74a494e51f
Егор Рындин <ryndinegr@yandex.ru>
2024-02-19 16:12:10 +0300

    Create model Tag

--- backend/foodgram/models.py
+++ backend/foodgram/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField('Название', max_length=200)
+    color = models.CharField('Цвет в НЕХ', max_length=7)
+    slug = models.SlugField('Уникальный слаг', unique=True, max_length=200)
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+
+    def __str__(self):
+        return self.name
