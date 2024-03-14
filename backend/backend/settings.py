d929e5bf125f2f1ca529b749baabefb6421b6bd2
Егор Рындин <ryndinegr@yandex.ru>
2024-03-14 11:30:05 +0300

    fix static

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -101,6 +101,8 @@ USE_TZ = True
 AUTH_USER_MODEL = 'users.User'
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
